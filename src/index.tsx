6938b1f791d40903f57741a37dc6c84b8f72563b
Steven Mokoena <steven@sizananitechnologies.com>
2022-05-03 20:03:56 +0200

    looks like a new react 18 implim? idk, just messing about

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css'
+import {createRoot} from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// @ts-ignore
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
